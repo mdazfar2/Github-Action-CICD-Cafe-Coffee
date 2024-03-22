96fcfabedc23b6628c79f01c4baaa8b258bfabcb
Md Azfar Alam <mdazfaralam440@gmail.com>
2024-03-22 15:48:23 +0530

    Delete Eks-terraform directory

--- Eks-terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "azfar-cafe-coffee"
-    key            = "EKS/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "eks-terraform-lock"
-  }
-}
